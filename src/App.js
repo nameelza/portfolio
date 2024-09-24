c80fc467f8aff6e6db27cffb50b0853a9223a6dc
nameelza <lizasvirschik@gmail.com>
2024-09-24 14:30:10 -0700

    react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages";
+import Portfolio from "./pages";
+import About from "./pages";
+import Contact from "./pages";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route>
+          <Route index element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
