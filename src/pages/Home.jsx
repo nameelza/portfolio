0e664f5c44d8c22c22a9fbefcf4444ada2364f61
nameelza <lizasvirschik@gmail.com>
2024-11-26 21:02:46 -0800

    sub-category added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import img1 from "../assets/mainImages/1.jpg";
 import img2 from "../assets/mainImages/2.JPG";
+import jewelryImg from "../assets/mainImages/jewelry.JPG";
+import foodImg from "../assets/mainImages/food.webp";
+import outdoorsImg from "../assets/mainImages/outdoors.jpg";
+import stillImg from "../assets/mainImages/still3.jpg";
 
 const Home = () => {
   return (
@@ -12,7 +16,24 @@ const Home = () => {
         </div>
       </div>
       <div className="main-container m-2"></div>
-      <div className="main-container m-3"></div>
+      <div className="main-container m-3">
+        <div className="sub-category">
+          <span>Still life</span>
+          <img src={stillImg} alt="mainImg" />
+        </div>
+        <div className="sub-category">
+          <span>Jewelry</span>
+          <img src={jewelryImg} alt="mainImg" />
+        </div>
+        <div className="sub-category">
+          <span>Food+Beverage</span>
+          <img src={foodImg} alt="mainImg" />
+        </div>
+        <div className="sub-category">
+          <span>Outdoors</span>
+          <img src={outdoorsImg} alt="mainImg" id="outdoors-img" />
+        </div>
+      </div>
     </div>
   );
 };
