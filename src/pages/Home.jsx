928a47ece6c3c20654826a102c83f7f3dc4361c6
nameelza <lizasvirschik@gmail.com>
2025-01-10 16:29:21 -0800

    homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,36 @@
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 import img1 from "../assets/mainImages/1.jpg";
 import img2 from "../assets/mainImages/2.JPG";
+import img3 from "../assets/mainImages/2.JPG";
+import img4 from "../assets/mainImages/2.JPG";
 import jewelryImg from "../assets/mainImages/jewelry.JPG";
 import foodImg from "../assets/mainImages/food.webp";
 import outdoorsImg from "../assets/mainImages/outdoors.jpg";
 import stillImg from "../assets/mainImages/still3.jpg";
 
 const Home = () => {
+  const images = [img1, img2];
+
+  const [currIndex, setCurrIndex] = useState(0);
+
+  console.log(currIndex);
+
+  useEffect(() => {
+    const switchImage = () => {
+      setCurrIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+    const interval = setInterval(switchImage, 1500);
+    console.log(currIndex, "curr");
+
+    return () => clearInterval(interval);
+  }, [images]);
+
   return (
     <div className="main">
       <div className="main-container m-1">
-        <img src={img1} alt="mainImg" />
+        <img src={images[currIndex]} alt="mainImg" />
         <div>
           <h1>Photographer&stylist based in San Francisco Bay Area</h1>
           <button>get in touch</button>
