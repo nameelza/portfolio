09d1cca409d64727760d398ed3d4eb03706672ca
nameelza <lizasvirschik@gmail.com>
2025-01-28 17:35:30 -0800

    closeMenu feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Dropdown from "./Dropdown";
 
-const NavbarLink = ({ to, label }) => (
+const NavbarLink = ({ to, label, closeMenu }) => (
   <NavLink
     to={to}
     className={({ isActive }) =>
       `nav-link ${isActive && "active"} ${(label =
         "portfolio" && "portfolio-link")}`
     }
+    onClick={closeMenu}
   >
     {label}
     {label === "PORTFOLIO" && <Dropdown />}
@@ -21,6 +22,11 @@ const Navbar = () => {
   const handleToggle = () => {
     setIsNavOpen((prevState) => !prevState);
   };
+
+  const closeMenu = () => {
+    setIsNavOpen(false);
+  };
+
   return (
     <nav>
       <div className="nav-left">
@@ -31,10 +37,10 @@ const Navbar = () => {
       </div>
       <div className="nav-right">
         <div className={`nav-links ${isNavOpen && "open"}`}>
-          <NavbarLink to="/" label="HOME" />
-          <NavbarLink to="/portfolio" label="PORTFOLIO" />
-          <NavbarLink to="/about" label="ABOUT" />
-          <NavLink to="/contact" className="contact-button">
+          <NavbarLink to="/" label="HOME" closeMenu={closeMenu} />
+          <NavbarLink to="/portfolio" label="PORTFOLIO" closeMenu={closeMenu} />
+          <NavbarLink to="/about" label="ABOUT" closeMenu={closeMenu} />
+          <NavLink to="/contact" className="contact-button" onClick={closeMenu}>
             CONTACT
           </NavLink>
         </div>
