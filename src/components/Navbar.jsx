de140d24aa9ce224dbe51e758055256f8da3fa14
nameelza <lizasvirschik@gmail.com>
2025-01-29 22:32:53 -0800

    styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,9 @@ const NavbarLink = ({ to, label, closeMenu }) => (
   <NavLink
     to={to}
     className={({ isActive }) =>
-      `nav-link ${isActive && "active"} ${(label =
-        "portfolio" && "portfolio-link")}`
+      `nav-link ${isActive && "active"} ${
+        label === "portfolio" && "portfolio-link"
+      }`
     }
     onClick={closeMenu}
   >
@@ -18,9 +19,18 @@ const NavbarLink = ({ to, label, closeMenu }) => (
 
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isNavClosing, setIsNavClosing] = useState(false);
 
   const handleToggle = () => {
-    setIsNavOpen((prevState) => !prevState);
+    if (isNavOpen) {
+      setIsNavClosing(true);
+      setTimeout(() => {
+        setIsNavOpen(false);
+        setIsNavClosing(false);
+      }, 300);
+    } else {
+      setIsNavOpen(true);
+    }
   };
 
   const closeMenu = () => {
@@ -36,7 +46,11 @@ const Navbar = () => {
         <span>commercial photography</span>
       </div>
       <div className="nav-right">
-        <div className={`nav-links ${isNavOpen && "open"}`}>
+        <div
+          className={`nav-links ${isNavOpen && "open"} ${
+            isNavClosing && "closing"
+          }`}
+        >
           <NavbarLink to="/" label="HOME" closeMenu={closeMenu} />
           <NavbarLink to="/portfolio" label="PORTFOLIO" closeMenu={closeMenu} />
           <NavbarLink to="/about" label="ABOUT" closeMenu={closeMenu} />
@@ -46,7 +60,7 @@ const Navbar = () => {
         </div>
       </div>
       <button
-        className={`hamburger ${isNavOpen ? "open" : ""}`}
+        className={`hamburger ${isNavOpen && !isNavClosing && "open"}`}
         onClick={handleToggle}
       >
         <span className="line"></span>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+
 import CategoryLink from "../components/CategoryLink";
 
 import img1 from "../assets/mainImages/1.jpg";
 import img2 from "../assets/mainImages/2.JPG";
-// simport img3 from "../assets/mainImages/2.JPG";
+// import img3 from "../assets/mainImages/2.JPG";
 // import img4 from "../assets/mainImages/2.JPG";
 import jewelryImg from "../assets/mainImages/jewelry.JPG";
 import foodImg from "../assets/mainImages/food.webp";
@@ -33,9 +35,11 @@ const Home = () => {
     <div className="main">
       <div className="main-container m-1">
         <img src={images[currIndex]} alt="Product Photography" />
-        <div>
+        <div className="main-text">
           <h1>Photographer&stylist based in San Francisco Bay Area</h1>
-          <button>get in touch</button>
+          <div className="button">
+            <NavLink to="/contact">Get in touch</NavLink>
+          </div>
         </div>
       </div>
       <div className="main-container m-2"></div>
