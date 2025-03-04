cb61a58a39f5f7fa603b9e501843cc3e5fa8feeb
nameelza <lizasvirschik@gmail.com>
2025-03-04 14:51:12 -0800

    scroll to top

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import ScrollToTop from "./components/ScrollToTop";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
@@ -9,6 +10,7 @@ import Contact from "./pages/Contact";
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
